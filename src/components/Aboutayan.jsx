6fada0f74b58e5c5406ec1d9c9d73752b14fa92d
ayan-plush <ayankhajuria2005@gmail.com>
2024-07-18 10:52:19 +0530

    about me fixed

--- src/components/Aboutayan.jsx
+++ src/components/Aboutayan.jsx
@@ -3,15 +3,17 @@ import React from 'react'
 function Aboutayan() {
   return (
     <div className='flex items-center'>
-        <div className='mt-[10vh]'>
-      <h1 className="w-full px-[5vw] font-['Founders_Grotesk_X-Condensed' src:'my-finalwebapp\public\fonts'] text-[1.5vw] ">
+        <div className='mt-[10vh] flex '>
+      <h1 className="w-2/3 mt-[5vw] px-[5vw] font-['Founders_Grotesk_X-Condensed' src:'my-finalwebapp\public\fonts'] text-[1.5vw] ">
       Hello ! I'm Ayan,<br></br> and I'm a second-year software engineering student at Delhi Technological University with a passion for technology, art, and literature.<br></br><br></br>
       Growing up, I was fascinated by artists and how they are able to captivate their audience's attention with ease.<br></br> I dreamt of presenting to an audience something that is bizarre, yet impeccably beautiful and innovative.
       This passion later developed into my love for digital-art and being able to create captivating and mesmerizing websites.<br></br><br></br>
       I love to read and practice my problem solving skills on leetcode. I'm currently an associate at my university's Queer collective, we aim
-      to foster an inclusive space and encourage LGBTQIA+ voices in STEM.
+      to foster an inclusive space and encourage LGBTQIA+ voices in STEM.<br></br>
       my email : ayan.fwd@gmail.com
       </h1>
+      <div className="w-[40vw] h-[40vw] bg-cover bg-[url('/assets/ayanpic.png')] bg-center">
+      </div>
       </div>
     </div>
   )
