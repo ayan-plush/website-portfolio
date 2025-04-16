76257a5145d92bce5259ef5c29f73e0cd2576bb9
ayan-plush <ayankhajuria2005@gmail.com>
2025-04-16 11:18:05 +0530

    about me changed

--- src/components/Aboutayan.jsx
+++ src/components/Aboutayan.jsx
@@ -5,10 +5,10 @@ function Aboutayan() {
     <div className='flex items-center'>
         <div className='mt-[10vh] flex '>
       <h1 className="w-2/3 mt-[5vw] px-[5vw] font-['Founders_Grotesk_X-Condensed' src:'my-finalwebapp\public\fonts'] text-[1.5vw] ">
-      Hello ! I'm Ayan,<br></br> and I'm a second-year software engineering student at Delhi Technological University with a passion for technology, art, and literature.<br></br><br></br>
-      Growing up, I was fascinated by artists and how they are able to captivate their audience's attention with ease.<br></br> I dreamt of presenting to an audience something that is bizarre, yet impeccably beautiful and innovative.
-      This passion later developed into my love for digital-art and being able to create captivating and mesmerizing websites.<br></br><br></br>
-      I love to read and practice my problem solving skills on leetcode.<br></br>
+      Hello ! I'm Ayan,<br></br> and I'm a third-year software engineering student at Delhi Technological University with a passion for technology, art, and literature.<br></br><br></br>
+      Growing up, I was captivated by developers who could create immersive user experiences and elegant code that seamlessly commanded users' attention and engagement.<br></br> I dreamt of presenting to an audience something that is bizarre, yet impeccably beautiful and innovative.
+      This passion later developed into my love for being able to create captivating and mesmerizing websites.<br></br><br></br>
+      <br></br>
       my email : ayan.fwd@gmail.com
       </h1>
       <div className="w-[40vw] h-[40vw] bg-cover bg-[url('/assets/ayanpic.png')] bg-center">
